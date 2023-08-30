ec464ad77cfb18c19eed17e59a9b0f7bbf275cea
Paulo Henrique <phsilva098@gmail.com>
2023-08-30 14:24:38 -0300

    enviando arquivos

---
+++ Targets/NutriRankKit/Tests/NutriRankKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class NutriRankKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("NutriRankKit", "NutriRankKit")
+    }
+}
---
+++ Targets/NutriRankUI/Tests/NutriRankUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class NutriRankUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("NutriRankUI", "NutriRankUI")
+    }
+}
