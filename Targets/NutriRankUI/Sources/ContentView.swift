10b3181011c4da0e5c93ff55f376b9644dcf88e4
Stephane <stephanegiraolinhares@gmail.com>
2023-09-18 11:47:16 -0300

    access for camera and galery

--- Targets/NutriRankUI/Sources/ContentView.swift
+++ Targets/NutriRankUI/Sources/ContentView.swift
@@ -1,12 +1,51 @@
 import SwiftUI
+import UIKit
+import Foundation
 
 public struct ContentView: View {
+
     public init() {}
 
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @State private var selectedImage: UIImage?
+    @State private var isImagePickerDisplay = false
+
     public var body: some View {
-        Text("Hello, World!")
-            .padding()
+        NavigationView {
+            VStack {
+
+                if selectedImage != nil {
+                    Image(uiImage: selectedImage!)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                        .frame(width: 300, height: 300)
+                } else {
+                    Image(systemName: "snow")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                        .frame(width: 300, height: 300)
+                }
+
+                Button("Camera") {
+                    self.sourceType = .camera
+                    self.isImagePickerDisplay.toggle()
+                }.padding()
+
+                Button("photo") {
+                    self.sourceType = .photoLibrary
+                    self.isImagePickerDisplay.toggle()
+                }.padding()
+            }
+            .navigationBarTitle("Demo")
+            .sheet(isPresented: self.$isImagePickerDisplay) {
+                ImagePickerView(selectedImage: self.$selectedImage, sourceType: self.sourceType)
+            }
+
+        }
     }
+
 }
 
 
