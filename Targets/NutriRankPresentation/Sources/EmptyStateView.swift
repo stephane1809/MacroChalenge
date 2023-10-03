8e209b57c4ccb1f84c1051de3ab5ac154e345fdf
Stephane Girão <stephanegiraolinhares@gmail.com>
2023-10-03 12:15:23 -0300

    text of creat group

--- Targets/NutriRankPresentation/Sources/EmptyStateView.swift
+++ Targets/NutriRankPresentation/Sources/EmptyStateView.swift
@@ -25,14 +25,25 @@ public struct EmptyStateView: View {
                             .scaledToFill()
                             .frame(width: metrics.size.width * 0.6, height: metrics.size.height * 0.25)
                     }
-                    HStack {
-                        Image(systemName: "person.crop.circle.fill.badge.plus").font(.system(size: 18, weight: .regular))
-                            .aspectRatio(contentMode: .fit)
-                            .scaledToFill()
 
-                        Text ("Criar grupo")
+                    VStack (alignment: .leading) {
+                        HStack {
+                            Image(systemName: "person.crop.circle.fill.badge.plus").font(.system(size: 18, weight: .regular))
+                                .aspectRatio(contentMode: .fit)
+                                .scaledToFill()
 
+                            Text ("Crie um grupo")
+                                .font(.headline)
+                        }
+                        Text("Você não possui grupos no momento. Crie num novo grupo e inicie novos hábitos alimentares com seus amigos!")
                     }
+                    .padding(.horizontal, 15)
+                    .padding(.vertical, 12)
+                    .frame(maxWidth: metrics.size.width * 0.92, minHeight: metrics.size.height * 0.09)
+                    .background(.white)
+                    .cornerRadius(10)
+                    .shadow(radius: 1, x: 0, y: 1)
+
 
                     VStack {
                         Button {
@@ -54,8 +65,12 @@ public struct EmptyStateView: View {
                         .buttonStyle(.bordered)
                     }
                 }
-
+                .frame(maxWidth: .infinity)
+                .background(Color("Light blue"))
             }
+
         }
+
     }
+
 }
